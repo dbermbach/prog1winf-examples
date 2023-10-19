6050f31907941bbb6d4c2a97177f4749763b7ea8
dbermbach <dbermbach@gmail.com>
2023-10-19 18:30:47 +0200

    add example from lecture on Oct 19

---
+++ src/berlin/tu/mcc/prog1/ws2324/FirstClass.java
@@ -0,0 +1,35 @@
+package berlin.tu.mcc.prog1.ws2324;
+
+import Prog1Tools.IOTools;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        String name;
+        name = "Jane Doe";
+        String name2 = "John Doe";
+        /*System.out.println(name);
+        System.out.print("Hello ");
+        System.out.println(name2);*/
+        name2 = "Hello " + name;
+        System.out.println(name2 + " and all the others");
+        // comment
+        long i = 42;
+        int j = (int) i;
+        System.out.println(i+(3*8/12)-515*i);
+        i = i*43;
+        i*=43;
+        long meineLieblingsLongVariable = 31;
+        System.out.println("1 durch 2 = "+ ((double)1/2));
+        System.out.println("1 durch 2 = "+ (1/2));
+        System.out.println("1 durch 2 = "+ (1/2.0));
+        System.out.println("1 mod 2 = " + (1%2));
+        double d = Math.sqrt(49);
+
+
+
+
+
+    }
+}
