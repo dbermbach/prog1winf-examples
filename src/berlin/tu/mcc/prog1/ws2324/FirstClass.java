b6f27d5b887286f8c5be479b77e2256b8483b2b2
dbermbach <dbermbach@gmail.com>
2023-10-24 21:05:20 +0200

    add example code from lecture on Oct 24

--- src/berlin/tu/mcc/prog1/ws2324/FirstClass.java
+++ src/berlin/tu/mcc/prog1/ws2324/FirstClass.java
@@ -26,6 +26,33 @@ public class FirstClass {
         System.out.println("1 durch 2 = "+ (1/2.0));
         System.out.println("1 mod 2 = " + (1%2));
         double d = Math.sqrt(49);
+        boolean b = meineLieblingsLongVariable<i;// > <= >= == !=
+        boolean b2 = false;
+        b2 = b == b2;
+        b2 = !b; // not
+        b2 = b && b2; // and
+        b2 = b || b2; // or
+
+        int c = 0;
+        c++;
+        ++c;
+        c--;
+        --c;
+        i = 0;
+        i = c++; // äquivalent zu: i=c;c=c+1;
+        System.out.println("i="+i +"c="+c);
+        i = 0; c =0;
+        i = ++c; // äquivalent zu; c=c+1;i=c;
+        System.out.println("i="+i+"c="+c);
+        meineLieblingsLongVariable++;
+
+        i = IOTools.readInt();
+        System.out.println(i++);
+        i = IOTools.readInt("bitte Zahl eingeben: ");
+        System.out.println("Echo: "+i);
+
+        
+
 
 
 
