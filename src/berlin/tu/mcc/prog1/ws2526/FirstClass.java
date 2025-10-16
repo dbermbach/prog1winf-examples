49513154a40c3c2005bb167158f1e65cc1699bb2
dbermbach <dbermbach@gmail.com>
2025-10-16 16:32:54 +0200

    add first class from Oct 16 lecture

---
+++ src/berlin/tu/mcc/prog1/ws2526/FirstClass.java
@@ -0,0 +1,17 @@
+package berlin.tu.mcc.prog1.ws2526;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.print("foo");
+        System.out.print("foo");
+        // einzeiliger Kommentar
+        /*
+        mehrzeiliger Kommentar
+         */
+        int i;
+        i=5;
+        int j = 6;
+    }
+}
