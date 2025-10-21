35b7b6fb6f13c10e51f676509b96f02fed42cd56
dbermbach <dbermbach@gmail.com>
2025-10-21 14:24:11 +0200

    add changes Oct 21, 2025

--- src/berlin/tu/mcc/prog1/ws2526/FirstClass.java
+++ src/berlin/tu/mcc/prog1/ws2526/FirstClass.java
@@ -1,5 +1,7 @@
 package berlin.tu.mcc.prog1.ws2526;
 
+import Prog1Tools.IOTools;
+
 public class FirstClass {
 
     public static void main(String[] args) {
@@ -13,5 +15,22 @@ public class FirstClass {
         int i;
         i=5;
         int j = 6;
+        //byte, short, int, long => Ganzzahlen
+        // float, double => Gleitkommazahlen
+        // boolean
+        // char
+        // String
+        String nameOfEmployee = "Jane Doe\n\t\"\\";
+        char c = 'a';
+        i = 0x42af; // hexadezimal
+        i = 0b1011; // binär
+        i = 123_456_789;
+        long l = 123l;
+        double d = 1.23; // .23 123e-56 => entspricht 123 mal zehn hoch minus 56
+        boolean b = true; // oder false
+        String s = null;
+        System.out.println(nameOfEmployee);
+        b = IOTools.readBoolean("bitte boolean eingeben");
+        System.out.println(b);
     }
 }
