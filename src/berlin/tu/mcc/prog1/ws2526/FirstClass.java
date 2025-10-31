8a7d016739c5a940aeb021ceb71bbd6d08e5fc75
dbermbach <dbermbach@gmail.com>
2025-10-31 12:12:11 +0100

    add changes Oct 30

--- src/berlin/tu/mcc/prog1/ws2526/FirstClass.java
+++ src/berlin/tu/mcc/prog1/ws2526/FirstClass.java
@@ -63,6 +63,7 @@ public class FirstClass {
         m = i = 1;
 
         double dRand = Math.ceil(Math.random());
+        //Google: java api klassenname
 
         if (dRand == 0) {
             System.out.println("oh, it was zero!1!111!");
@@ -116,6 +117,42 @@ public class FirstClass {
             default:
                 System.out.println("i was " +i);
         }
+
+
+        int [] arr; // leere Kiste hier
+        arr = new int [3]; //jetzt Storagebox angemietet
+        arr[0] = 42; // Indizes von 0 bis 2
+        arr[1] = 43;
+        arr[2] = 45;
+        int [] arr2 = new int [84838];
+        arr2 = arr;
+        arr2[0] = 1;
+        System.out.println(arr[0]);
+
+        //tiefe Kopie
+        int [] deepcopy = new int[arr.length];
+        deepcopy[0] = arr[0];
+        deepcopy[1] = arr[1];
+        deepcopy[2] = arr[2];
+        //System.out.println(arr[-34]);
+
+        int [][] arr2d = new int[2][3];
+        arr2d[0][0] = 42;
+        System.out.println(arr2d.length);
+        System.out.println(arr2d[1].length);
+        arr2d[0] = new int[4];
+
+        arr = new int[]{1, 2, 3, 4, 5};
+        int[] arr3 = {1,2,3};
+        System.out.println(arr[0]);
+        int [][] arr2dInOneLine = {{1,2,3}, {42,43,46},{1,2,3,4,5}};
+
+        deepcopy = new int[arr.length];
+        for (int ii=0;ii<arr.length;ii++){
+            deepcopy[ii] = arr[ii];
+        }
+        
+
 
 
     }
